3b09ed6b1eab24046b37c178545352c34b37db45
Scott Hardie <scottrmhardie@gmail.com>
2026-02-27 22:59:02 -0500

    xxz

--- include/requiem/debugger.hpp
+++ include/requiem/debugger.hpp
@@ -89,6 +89,7 @@ public:
   // Step operations
   virtual std::optional<StateSnapshot> StepForward() = 0;
   virtual std::optional<StateSnapshot> StepBackward() = 0;
+  virtual std::optional<StateSnapshot> StepOver() = 0;
 
   // -------------------------------------------------------------------------
   // Inspection
